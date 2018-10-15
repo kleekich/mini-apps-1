d0fd4ca7574bf96e9088b2debc48b058d34fb230
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-15 14:37:40 -0700

    Implement onclick listener to boxes

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,26 +1,31 @@
+var app = function() {
+
+	var board = [];
+	var isEnd;
+	var winner;
+	var turn;
+	var count;
 
 
-let app = function() {
-	let board = [
-					[0, 0, 0],
-					[0, 0, 0],
-					[0, 0, 0]
-				];
-	let isEnd = false;
-	let winner= undefined;
-	let turn = 1;
-
-	let initialize = function(){
+	var initialize = function(){
 		//For each td, add click event listener(clickBox)
-		board = [
+		this.board = [
 					[0, 0, 0],
 					[0, 0, 0],
 					[0, 0, 0]
 				];
-		isEnd = false;
-		winner= undefined;
-		turn = 1;
-		var boxes = document.querySelectorAll('.td');
+		this.isEnd = false;
+		this.winner= undefined;
+		this.turn = 1;
+		this.count = 0;
+
+		console.log('in initialze');
+		//Setup for game status and turn
+		document.body.querySelector('.gameStatus').innerText = 'New Game Started!';
+		document.body.querySelector('.turn').innerHTML = 'Turn: Player' + turn;
+
+		//Setup for boxes: put click event listener to each box
+		var boxes = document.body.querySelectorAll('td');
 		boxes.forEach((box, index)=>{
 			box.addEventListener("click", ()=>{
 				clickBox(index);
@@ -28,22 +33,35 @@ let app = function() {
 		})
 	}
 
-	let clickBox = function(index){
+	var clickBox = function(index){
+		console.log('clicked: ' + index);
 		//get row and col index
-		let row = index/3;
-		let col = index%3;
+		var row = Math.floor(index/3);
+		var col = Math.floor(index%3);
 
 		//checks whether it is a valid move with isValid
 		if(isValidMove(row, col) && !isEnd){
 			//update matrix 
-			board[row][col] = currentPlayer
+			this.board[row][col] = turn;
 			//update view
-			updateBoardView(row, col, currentPlayer);
+			updateBoardView(row, col, turn);
 			//check for winning move
 			if(isWinningMove(row, col)){
 				isEnd = true;
-				updateGameView(currentPlayer);
+				winner = turn;
+				updateGameViewForWinner(turn);
 			}
+
+			count++;
+			//check for tie
+			if(count === 9){
+				updateGameViewForWinner(0);
+			}
+			turn = turn===1? 2 : 1;
+			
+
+
+
 		//If it is not a valid move
 		}else{
 			prompt("Cick a valid box please");
@@ -51,32 +69,58 @@ let app = function() {
 	}
 
 	//function isValid()
-	let isValidMove(row, col){
-		return board[row][col] === 0 && !isEnd;
+	var isValidMove = function(row, col){
+		return this.board[row][col] === 0 && !isEnd;
 	}
 	//check for winning
-	let isWinningMove(row,col){
+	var isWinningMove = function(row,col){
 		if(isRowComplete(row, col)||
 		   isColComplte(row, col)||
 		   isDiagonalComplete(row, col)
 		) return true;
 	}
-	let isRowComplete(row, col){
-		let row = board[row];
+	var isRowComplete = function(row, col){
+		let row = this.board[row];
 		return _.reduce(row, (accum, col)=>{
-			return accum && col === currentPlayer;
+			return accum && col === turn;
 		}, true)
 	}
 
-	let isColComplete(row, col){
+	var isColComplete = function(row, col){
 		var completed = true;
 		for(let i = 0; i <3; i++){
-			completed = completed && board[i][col] === currentPlayer;
+			completed = completed && this.board[i][col] === turn;
 		}
 		return completed;
 	}
-}
 
+
+	var updateBoardView = function(row, col){
+	//Make a query string
+	var query = '.row'+row+ ' .col'+col;
+	//var query = '.row1 .col1';
+	//Select the box
+	var selectedBox = document.body.querySelector(query);
+
+		//Change View
+		selectedBox.innerText = marker();
+	}
+
+	//function updateGameView
+	//var updateGameViewForWinner = function()
+
+	//marker returns correct marker for the currentPlayer
+	var marker = function(){
+		return turn === 1 ? 'X' :'O';
+	}
+
+
+
+
+
+	initialize();
+}
+app();
 
 
 
@@ -91,11 +135,15 @@ let app = function() {
 
 
 //View
+
 //function updateBoardView
-//function updateGameView
+
+
 
 //TODOs
 /*
 	1. install underscore
 	2. turn
 */
+
+
