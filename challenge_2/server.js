604b158586d3c96e1c36cf09636fdd390fbda165
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-16 11:23:30 -0700

    Initialize server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+//Create an express application
+var express = require('express');
+var app = express();
 
+
+app.get('/', function(req, res){
+	res.send("Hello Breeze!");
+});
+
+var port = 3000;
+app.listen(port, () => console.log(`Listening to port ${port}`));
 
