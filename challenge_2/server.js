7b610c55c9532a9f9dd8663fb99a4a454b30812c
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-16 15:36:11 -0700

    Implement buildCSV function to generate CSV format string

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,19 +1,72 @@
 //Create an express application
 var express = require('express');
-var path = require('path');
+//var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
 //Use express's use function to serve with static files
 app.use(express.static('client'));
 
+//Use body parser to get entire body portion of an incoming request on req.body
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded());
+
+
 // app.get('/', function(req, res){
 // 	//Serve index.html file
 // 	//res.sendFile(path.join(__dirname + '/index.html'));
 // });
 
+
 app.post('/', function(req, res){
-	console.log(req.body.userInput);
+	//Creating JSON string ommiting ';' at the end
+	var jsonStr = req.body.userInput;
+	jsonStr = jsonStr.substr(0,jsonStr.length-1);
+	var objectJSON = JSON.parse(jsonStr);
+
+	
+
+	var buildCSV = function(objectJSON){
+		//Aggregate arra
+		var aggregateArr = [];
+		//create a stack for DFS in nested object 
+		var stack = [];
+		//push root Object
+		stack.push(objectJSON)
+		while(stack.length!==0){
+			//get current object
+			var currObject = stack.pop();
+
+			//Add children to stack
+			for(var child of currObject.children){
+				stack.push(child);
+			}
+			//build a line
+			var newLineArr = [];	
+			for(var key in currObject){
+				if(key === 'children'){
+					//get rid of comma at the end and add new line
+					newLineArr.pop();
+					newLineArr.push('\n');
+				}else{
+					//****check whether it is a string
+					newLineArr.push(currObject[key]);
+					newLineArr.push(',')	
+				}
+			}
+			//Add joined newLine String to whole
+			aggregateArr.push(newLineArr.join(''));
+		}
+		return aggregateArr.join('');
+	}
+
+	var csv = buildCSV(objectJSON);
+	console.log('------------------------------------');
+	console.log(csv);
+	console.log('------------------------------------');
+
 	res.redirect('/');
 })
 
