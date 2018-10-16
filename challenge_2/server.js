bf42802749f5cc5bf76671d36c5b93fb0e409a00
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-16 12:03:54 -0700

    Add bodyParser

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,21 @@
 //Create an express application
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
+//Use express's use function to serve with static files
+app.use(express.static('client'));
 
-app.get('/', function(req, res){
-	res.send("Hello Breeze!");
-});
+// app.get('/', function(req, res){
+// 	//Serve index.html file
+// 	//res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+app.post('/', function(req, res){
+	console.log(req.body.userInput);
+	res.redirect('/');
+})
 
 var port = 3000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
