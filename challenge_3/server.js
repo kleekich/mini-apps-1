8467719c2601ece96c7705782b794b56fd128370
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-17 11:13:09 -0700

    Implement initialization server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+var express = require('express');
 
+var app = express();
+
+
+app.use(express.static('client'));
+
+
+var port = 3000;
+
+app.listen(port, ()=>{
+	console.log('Listening to port ', port);
+})
 
