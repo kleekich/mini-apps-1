e9c4badc4ebc0f8bd6f57980845f86a6a90d97a7
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-17 16:45:04 -0700

    Add App class

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,17 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
 
+class App extends React.Component{
 
+}
+
+const home = (
+	<div>
+		<h1>Welcom To Checkout Form!</h1>
+		<button>Checkout</button>
+	</div>
+)
+
+console.log("HI FROM app.jsx");
+
+ReactDOM.render(home, document.getElementById('root'));
