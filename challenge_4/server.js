069ab34acb20d639fad8668461e1004cb1bb00f3
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-18 15:39:05 -0700

    Change static file to serve client/dist

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -4,7 +4,7 @@ var app = express();
 
 var port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('client/dist'));
 
 app.listen(port, ()=>{
 	console.log('listening to port: ', port);
