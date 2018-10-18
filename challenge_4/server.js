e60352f4208935b3e615e0845475674b2d81fb6e
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-18 14:20:51 -0700

    Implement boilerplate server code

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,12 @@
+var express = require('express');
 
+var app = express();
+
+var port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, ()=>{
+	console.log('listening to port: ', port);
+})
 
